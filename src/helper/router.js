8987e116e3dbe66b103ee964ace96184a69cc32f
songchuwang <3302295966@qq.com>
2018-11-01 11:07:49 +0800

    finish gzip and deflate file

--- src/helper/router.js
+++ src/helper/router.js
@@ -5,6 +5,8 @@ const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 const config = require('../config/defaultConfig'); //使用require可以放心使用相对路径
+const mime = require('./mime');
+const compress = require('./compress');
 
 const tplPath = path.join(__dirname, '../template/dir.tpl');
 // 读取tpl文件，是Buffer形式
@@ -16,10 +18,15 @@ module.exports = async function (req, res, filePath) {
     try {
         const stats = await stat(filePath);
         if (stats.isFile()) {
+            const contentType = mime(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
+            res.setHeader('Content-Type', `${contentType};charset=UTF-8`);
             // 文件通过流的形式读出来，然后一点点吐回给客户端
-            fs.createReadStream(filePath).pipe(res);
+            let rs = fs.createReadStream(filePath);
+            if(filePath.match(config.compress)){
+              rs = compress(rs,req,res);
+            }
+            rs.pipe(res);
 
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath);
